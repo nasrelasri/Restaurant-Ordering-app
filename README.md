e7d15b918bb46bd70e78a6b0c91533f2a1fa98cf
Nasr El Asri <nasr.asri10@gmail.com>
2025-08-21 15:25:11 +0100

    set up initial structure and design

---
+++ README.md
@@ -0,0 +1,38 @@
+# Restaurant Ordering App
+
+A simple web-based restaurant ordering mobile app where users can browse menu items, add/remove the items, and complete the order.  
+Built with **HTML, CSS, and JavaScript**.
+
+## Features
+- Browse menu items
+- Add/remove items
+- View order summary and total price
+- Confirmation message after placing an order
+
+## Demo
+[App Screenshots](https://www.figma.com/design/p9HI6xNOtH7VdYB6ChqqWI/Mobile-Restaurant-Menu--Copy-?node-id=0-1&p=f&t=TCROTLr3wcyTbxVy-0)  
+
+## Technologies Used
+- HTML  
+- CSS
+- JavaScript
+
+## Project Structure
+    ├── index.html
+    ├── style.css
+    ├── script.js
+    ├── images/
+
+## Installation / Usage
+1. Clone the repo
+   ```bash
+   git clone https://github.com/nasrelasri/Restaurant-Ordering-app.git
+2. Open index.html in your browser
+
+## Future Improvements
+- Offer a "meal deal" discount
+- Allow users to rate their experience
+
+## License
+This project is licensed under the **MIT License**.
+Feel free to use, modify, and distribute this project as you like.
---
+++ script.js
@@ -0,0 +1,81 @@
+        // <!-- Menu Section -->
+        // <section class="menu-section">
+
+        //     <div id="menu-items" class="menu-items" role="list">
+
+        //         <!-- Menu items will be rendered here by JavaScript -->
+
+        //         <article class="menu-item" role="listitem">
+        //             <img src="./images/pizza.png" alt="Pizza." class="menu-item-image">
+        //             <div class="menu-item-details">
+        //                 <h3 class="menu-item-name">Pizza</h3>
+        //                 <p class="menu-item-ingredients">pepperoni, mushroom, mozzarella</p>
+        //                 <p class="menu-item-price">$14</p>
+        //             </div>
+        //             <button class="menu-item-add-btn">+</button>
+        //         </article>
+            
+        //         <article class="menu-item" role="listitem">
+        //             <img src="./images/hamburger.png" alt="Hamburger" class="menu-item-image">
+        //             <div class="menu-item-details">
+        //                 <h3 class="menu-item-name">Hamburger</h3>
+        //                 <p class="menu-item-ingredients">beef, cheese, lettuce</p>
+        //                 <p class="menu-item-price">$12</p>
+        //             </div>
+        //             <button class="menu-item-add-btn">+</button>
+        //         </article>
+            
+        //         <article class="menu-item" role="listitem">
+        //             <img src="./images/beer.png" alt="Beer" class="menu-item-image">
+        //             <div class="menu-item-details">
+        //                 <h3 class="menu-item-name">Beer</h3>
+        //                 <p class="menu-item-ingredients">grain, hops, yeast, water</p>
+        //                 <p class="menu-item-price">$12</p>
+        //             </div>
+        //             <button class="menu-item-add-btn">+</button>
+        //         </article>
+
+        //     </div>
+
+        // </section>
+
+
+
+
+
+
+
+
+
+
+
+        // <!-- Order Section -->
+        // <section id="order-section" class="order-section">
+
+        //     <h2>Your order</h2>
+        //     <div class="order-items" id="order-items" role="list">
+
+        //         <!-- Order items will be rendered here by JavaScript -->
+
+        //         <div class="order-item" role="listitem">
+        //             <span>Pizza</span>
+        //             <button class="remove-btn">remove</button>
+        //             <span class="order-item-price">$14</span>
+        //         </div>
+                
+        //         <div class="order-item" role="listitem">
+        //             <span>Beer</span>
+        //             <button class="remove-btn">remove</button>
+        //             <span class="order-item-price">$12</span>
+        //         </div>
+
+        //     </div>
+
+        //     <div class="order-total">
+        //         <span>Total price:</span>
+        //         <span id="total-amount" aria-label="Total amount">$0.00</span>
+        //     </div>
+
+        //     <button id="complete-order-btn" class="complete-order-btn">Complete order</button>
+
+        // </section>
